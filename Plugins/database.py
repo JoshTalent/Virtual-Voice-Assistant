7a1d1fd8df5a3a18710f94a79237b3ac6201c5aa
ntwari josue <134047313+JoshTalent@users.noreply.github.com>
2026-03-29 23:52:31 +0200

    Fix formatting in database.py print statements

--- Plugins/database.py
+++ Plugins/database.py
@@ -22,6 +22,7 @@ def get_data():
     print("{:<14} {:<79} {:<20}".format(table_head[0], table_head[1], table_head[2]))
     
     print()
+
     
     for row in data:
         print("{:<14} {:<79} {:<20}".format(row[0], row[1], row[2]))
