570ef9d7e4b5dba2fefd07da19bd61f1ef7daddc
ntwari josue <134047313+JoshTalent@users.noreply.github.com>
2026-04-14 20:04:18 +0200

    Update database.py

--- Plugins/database.py
+++ Plugins/database.py
@@ -8,6 +8,7 @@ conn = sqlite3.connect('../Data/chats.db')
 # Creating a cursor object using the cursor() method
 cursor = conn.cursor()
 
+
 def add_data(query):
     table = "INSERT INTO ASSISTANT(QUERY, DATE_TIME) VALUES (?, datetime('now', 'localtime'))"
     cursor.execute(table, (query,))
