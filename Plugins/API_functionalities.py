67e7929a3bd0a5c9190887ad26cfa33d5fc5942e
ntwari josue <134047313+JoshTalent@users.noreply.github.com>
2026-04-30 21:40:51 +0200

    Fix indentation and add popular TV series function

--- Plugins/API_functionalities.py
+++ Plugins/API_functionalities.py
@@ -90,6 +90,7 @@ def get_popular_movies():
     except KeyError:
         return None
 
+
 def get_popular_tvseries():
     try:
         response = requests.get(f"https://api.themoviedb.org/3/tv/popular?api_key={TMDB}&region=IN&sort_by=popularity.desc&"
