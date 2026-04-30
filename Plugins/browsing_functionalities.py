304373f0d1de4b78d56a37a2ec61f1d5edf445fc
ntwari josue <134047313+JoshTalent@users.noreply.github.com>
2026-04-30 21:37:01 +0200

    Fix query formatting in youtube function

--- Plugins/browsing_functionalities.py
+++ Plugins/browsing_functionalities.py
@@ -23,6 +23,7 @@ def youtube(query):
 	query = query.replace('play', ' ')
 	query = query.replace('on youtube', ' ')
 	query = query.replace('youtube', ' ')
+
 	
 	print("Searching for videos...")
 	videosSearch = VideosSearch(query, limit=1)
