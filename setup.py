2d88b63a407be5e5250677f1365e89a4278a5979
ntwari josue <134047313+JoshTalent@users.noreply.github.com>
2026-09-14 11:43:11 +0200

    Remove empty line in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ import subprocess
 
 print("THIS MAY TAKE A WHILE DEPENDING ON YOUR SYSTEM AND INTERNET SPEED\n\nPLEASE WAIT..\n\n")
 
-
 try:
     subprocess.run(["pip", "install", "-r", "requirements.txt"])
 except KeyboardInterrupt:
