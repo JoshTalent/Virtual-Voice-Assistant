3cd1953d191c71ec34904ea15b89a7976c8eb6f8
ntwari josue <134047313+JoshTalent@users.noreply.github.com>
2026-09-14 11:42:02 +0200

    Add try-except block for pip install command

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import subprocess
 
 print("THIS MAY TAKE A WHILE DEPENDING ON YOUR SYSTEM AND INTERNET SPEED\n\nPLEASE WAIT..\n\n")
 
+
 try:
     subprocess.run(["pip", "install", "-r", "requirements.txt"])
 except KeyboardInterrupt:
